87179c997bb924a40ceef68cd7fa9ad2c76ed885
yuya yamada <yuya19920721@gmail.com>
2021-07-16 11:13:44 +0900

    selfの学習

--- drink.rb
+++ drink.rb
@@ -2,7 +2,7 @@ class Drink
   attr_reader :name, :price
 
   def self.cola
-    new(120, 'コーラ').to_info
+    new(120, 'コーラ').to_info # self.new(price, name) = Drink.new(price, name) = new ここでのselfはたい焼きの型 + コーラ味
   end
 
   def self.redbull
@@ -20,5 +20,9 @@ class Drink
 
   def to_info
     { name: @name, price: @price, stock: 5 }
+    # ここでの self は実体化したもの  たい焼き自身
+
+    # メソッド自体はたい焼きを売る
+    # 実体化した後に使える技
   end
 end
